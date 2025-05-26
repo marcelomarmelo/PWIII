a8b66fe53deed7a8df4a06f872d74a061a712143
Marcelo <marcelomatheospelizario@gmail.com>
2025-05-26 18:05:00 -0300

    disciplinas razor mvc, inserção no banco de dados pelo controller do disciplinas e get set no disciplina entidade.

--- PW3/Controllers/UsuariosController.cs
+++ PW3/Controllers/UsuariosController.cs
@@ -10,7 +10,37 @@ namespace PW3.Controllers
         private readonly string connectionString = "Server=localhost;Database=aulabd2;Uid=root;Pwd=;";
         public IActionResult Index()
         {
-            
+
+            List<UsuarioEntidade> model = new List<UsuarioEntidade>();
+
+            using var connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            var cmd = new MySqlCommand("SELECT id, email FROM usuarios", connection);
+            using var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                UsuarioEntidade u = new UsuarioEntidade();
+                u.Id = reader.GetInt32("id");
+                u.Email = reader.GetString("email");
+                model.Add(u);
+            }
+            connection.Close();
+            return View(model);
+
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Detalhes()
+        {
+            return View();
+        }
+        public IActionResult Lista()
+        {
             List<UsuarioEntidade> model = new List<UsuarioEntidade>();
 
             using var connection = new MySqlConnection(connectionString);
@@ -29,5 +59,5 @@ namespace PW3.Controllers
             connection.Close();
             return View(model);
         }
+        }
     }
-}
